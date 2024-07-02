b3df5c6f3823bb76496eab45f875c2b1894bfc83
Marcos Glez <marcosgp142@gmail.com>
2024-07-02 12:58:31 +0000

    Initialized dim_salesbydate.sql

--- models/intermediate/dim_salesbydate.sql
+++ models/intermediate/dim_salesbydate.sql
@@ -1,8 +1,13 @@
-WITH salesbydate AS (
+    SELECT
     o_orderkey AS orderkey,
     o_orderstatus AS status,
-    o_totalprice AS totalprice,
+    l_extendedprice * (1 - l_discount) * (1 + l_tax) AS totalprice,
     o_orderdate AS orderdate,
     o_clerk AS clerk,
-    
-)
+    CASE l_returnflag
+        WHEN 'R' THEN 'Returned'
+        WHEN 'N' THEN 'Not returned'
+    END AS return_flag
+    FROM RAW_PROBOOTCAMP.RAW.RAW_ORDERS
+    JOIN RAW_PROBOOTCAMP.RAW.RAW_LINEITEM ON o_orderkey = l_orderkey
+    ORDER BY orderdate
