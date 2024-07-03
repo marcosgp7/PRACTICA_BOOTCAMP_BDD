9f81a7f36375b1caf4774399bdc6a08e85409540
Marcos Glez <marcosgp142@gmail.com>
2024-07-03 07:02:37 +0000

    Modificated salesbydate and stg_orders

--- models/intermediate/dim_salesbydate.sql
+++ models/intermediate/dim_salesbydate.sql
@@ -7,7 +7,12 @@
     CASE l_returnflag
         WHEN 'R' THEN 'Returned'
         WHEN 'N' THEN 'Not returned'
-    END AS return_flag
-    FROM RAW_PROBOOTCAMP.RAW.RAW_ORDERS
-    JOIN RAW_PROBOOTCAMP.RAW.RAW_LINEITEM ON o_orderkey = l_orderkey
+    END AS return_flag,
+    c_name AS customer_name,
+    p_partkey AS partkey,
+    p_name AS part_name
+    FROM {{ ref('stg_orders') }}
+    JOIN {{ ref('stg_lineitem') }} ON o_orderkey = l_orderkey
+    JOIN {{ ref('stg_customers') }} ON o_custkey = c_custkey
+    JOIN {{ ref('stg_part') }} ON l_partkey = p_partkey
     ORDER BY orderdate
--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -2,5 +2,15 @@ WITH stg_orders AS (
     SELECT *
     FROM RAW_PROBOOTCAMP.RAW.RAW_ORDERS
 )
-    SELECT *
+    SELECT o_orderkey, o_custkey,
+    o_orderstatus, o_totalprice,
+    o_orderdate, o_orderpriority,
+    CAST(REGEXP_REPLACE(o_clerk, '[^0-9]', '') AS NUMBER) AS o_clerk,
+    /*
+    En esta parte, se utiliza una expresión regular --> '[^0-9]', ''
+    Lo que está entre corchetes quiere decir: 'Cualquier carácter que no
+    sea un dígito' y lo que hace es eliminarlo, al ir seguido por la cadena
+    de reemplazo --> ''
+    */
+    o_shippriority, o_comment
     FROM stg_orders
