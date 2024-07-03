31824ea8a3983a4afe27fedc939bd4e4534fbd98
Marcos Glez <marcosgp142@gmail.com>
2024-07-03 08:19:00 +0000

    advances

--- models/intermediate/dim_salesbydate.sql
+++ models/intermediate/dim_salesbydate.sql
@@ -1,18 +1,18 @@
+WITH salesbydate AS (
     SELECT
-    o_orderkey AS orderkey,
-    o_orderstatus AS status,
-    l_extendedprice * (1 - l_discount) * (1 + l_tax) AS totalprice,
-    o_orderdate AS orderdate,
-    o_clerk AS clerk,
-    CASE l_returnflag
-        WHEN 'R' THEN 'Returned'
-        WHEN 'N' THEN 'Not returned'
-    END AS return_flag,
-    c_name AS customer_name,
-    p_partkey AS partkey,
-    p_name AS part_name
-    FROM {{ ref('stg_orders') }}
-    JOIN {{ ref('stg_lineitem') }} ON o_orderkey = l_orderkey
-    JOIN {{ ref('stg_customers') }} ON o_custkey = c_custkey
-    JOIN {{ ref('stg_part') }} ON l_partkey = p_partkey
-    ORDER BY orderdate
+        o_orderkey, o_custkey,
+        o_orderstatus, o_totalprice,
+        o_orderdate, o_orderpriority,
+        o_clerk, o_quantity,
+        l.l_quantity AS quantity,
+        ROUND(l.l_extendedprice * (1 - l.l_discount) * (1 + l.l_tax), 2) AS totalprice,
+        CASE l.l_returnflag
+            WHEN 'R' THEN 'Returned'
+            WHEN 'N' THEN 'Not returned'
+        END AS return_flag
+    FROM {{ ref('stg_orders') }} o
+    JOIN {{ ref('stg_lineitem') }} l ON o.o_orderkey = l.l_orderkey
+    WHERE l.l_returnflag <> 'A'
+)
+SELECT * FROM salesbydate
+ORDER BY orderdate
--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -2,9 +2,12 @@ WITH stg_orders AS (
     SELECT *
     FROM RAW_PROBOOTCAMP.RAW.RAW_ORDERS
 )
-    SELECT o_orderkey, o_custkey,
-    o_orderstatus, o_totalprice,
-    o_orderdate, o_orderpriority,
+    SELECT o_orderkey,
+    o_custkey,
+    o_orderstatus,
+    o_totalprice,
+    o_orderdate,
+    o_orderpriority,
     CAST(REGEXP_REPLACE(o_clerk, '[^0-9]', '') AS NUMBER) AS o_clerk,
     /*
     En esta parte, se utiliza una expresión regular --> '[^0-9]', ''
@@ -12,5 +15,4 @@ WITH stg_orders AS (
     sea un dígito' y lo que hace es eliminarlo, al ir seguido por la cadena
     de reemplazo --> ''
     */
-    o_shippriority, o_comment
     FROM stg_orders
